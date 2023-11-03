7d03ff725bd28ed7596bb82a06b9ee521381bab4
Stephanie-Y-Lopez <145513219+Stephanie-Y-Lopez@users.noreply.github.com>
2023-11-02 21:15:21 -0700

    seconddraft

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -72,7 +72,44 @@ namespace Lab.Assignment.Stephanie.Lopez
 
         private void AverageResultBut_Click(object sender, RoutedEventArgs e)
         {
-           
+            //string number1 = TextBox1.Text;
+            //string number2 = TextBox2.Text;
+            //string number3 = TextBox3.Text;
+            //string number4 = TextBox4.Text;
+            //int num1 = int.Parse(number1);
+            //int num2 = int.Parse(number2);
+            //int num3 = int.Parse(number3);
+            //int num4 = int.Parse(number4);
+            //string equation = Result1 + Result2 / 2;
+            string result1 = Result1.Content.ToString();
+            string result2 = Result2.Content.ToString();
+
+            int num1 = int.Parse(result1);
+            int num2 = int.Parse(result2);
+            int number = 10;
+
+            // Concatenating = ADD STRINGS TOGETHER
+            // 2 ints : 1 + 1 = 2
+            // 2 string : "1" + "1" = "11"
+            // "cat" + "dog" = "catdog"
+
+            // int, short, long = integers = WHOLE NUMBERS
+            // float, double, decimal = Floating point = NUMBERS WITH DECIMAL POINTS
+
+            // 5 + 5 / 5 != 2
+            // 5 + 1 = 6
+
+            // (5 + 5) / 2 = 2
+
+
+            double equation =  (num1 + num2)  / 2;
+            //string e2 = ((num1 + num2) / 2).ToString();
+
+            Result3.Content = equation;
+
+            // pemdas (perethesis exponents mutiplication divsion add subtract)
+
+            // Result3.Content = equation;
         }
     }
 }
